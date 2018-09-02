19bb5dbe554c74ca7d6245e6f2f997f6bc627de3
Mahesh Vangala <vangalamaheshh@gmail.com>
2018-09-02 16:59:00 -0400

    removed in and out file path - helpful to test on spark/hadoop yarn

--- variant-caller/src/main/java/pipelines/variant_caller/VariantCaller.java
+++ variant-caller/src/main/java/pipelines/variant_caller/VariantCaller.java
@@ -1,12 +1,17 @@
 package pipelines.variant_caller;
 
+import java.util.List;
+import java.util.Arrays;
+
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
 //import org.apache.beam.sdk.coders.BigEndianIntegerCoder;
 //import org.apache.beam.sdk.coders.KvCoder;
 //import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.ParDo;
 
 import pipelines.variant_caller.AddLines;
@@ -22,10 +27,17 @@ public class VariantCaller
     {
         PipelineOptions opts = PipelineOptionsFactory.fromArgs(args).create();
         Pipeline p = Pipeline.create(opts);
-        PCollection<String> lines = p.apply(TextIO.read().from("test_in.csv"));
+        // Create a Java Collection, in this case a List of Strings.
+        final List<String> LINES = Arrays.asList(
+          "sample1,params1",
+          "sample2,params2",
+          "sample3,params3");
+        //PCollection<String> lines = p.apply(TextIO.read().from("hdfs://test_in.csv"));
+        // Apply Create, passing the list and the coder, to create the PCollection.
+        PCollection<String> lines = p.apply(Create.of(LINES)).setCoder(StringUtf8Coder.of());
         PCollection<String> outLines = lines.apply(ParDo.of(new LaunchDocker.LaunchJobs()));
         PCollection<String> mergedLines = outLines.apply(Combine.globally(new AddLines()));
-        mergedLines.apply(TextIO.write().to("test_out.csv"));
+        //mergedLines.apply(TextIO.write().to("hdfs://test_out.csv"));
         p.run();
     }
 }
